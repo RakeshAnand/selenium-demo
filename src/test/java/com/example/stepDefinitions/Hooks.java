636807e35af29a01501427e87f644910317b7c44
Rakesh Anand <rakeshanand2008@gmail.com>
2026-05-01 02:57:02 +0530

    Extent Report

--- src/test/java/com/example/stepDefinitions/Hooks.java
+++ src/test/java/com/example/stepDefinitions/Hooks.java
@@ -1,20 +1,31 @@
 package com.example.stepDefinitions;
 
+import com.example.utils.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.openqa.selenium.WebDriver;
-import com.example.utils.DriverFactory;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario; //  import Scenario
+import org.openqa.selenium.OutputType; //  import OutputType
+import org.openqa.selenium.TakesScreenshot; //  import TakesScreenshot
 
 public class Hooks {
-    private WebDriver driver;
 
     @Before
     public void setUp() {
-        driver = DriverFactory.getDriver();
+        DriverFactory.getDriver(); // initialize driver once
     }
 
     @After
     public void tearDown() {
-        DriverFactory.quitDriver();
+        DriverFactory.quitDriver(); // quit driver once
+    }
+
+    @AfterStep
+    public void addScreenshot(Scenario scenario) {
+        if (scenario.isFailed()) {
+            final byte[] screenshot = ((TakesScreenshot) DriverFactory.getDriver())
+                    .getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", "Failed Step Screenshot");
+        }
     }
 }
