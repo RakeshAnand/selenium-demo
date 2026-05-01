7a757d81a0c395786ae053f074c723f5c1fc7aa9
Rakesh Anand <rakeshanand2008@gmail.com>
2026-05-01 12:20:18 +0530

    pom

--- src/test/java/com/example/runners/TestRunner.java
+++ src/test/java/com/example/runners/TestRunner.java
@@ -4,9 +4,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features", glue = "com.example.stepDefinitions", plugin = {
-                "pretty",
-                "json:target/cucumber.json",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        "pretty",
+        "json:target/cucumber.json",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 }, monochrome = true)
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
